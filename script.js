cb8867ec977ff8e466c83a5bf1b199b23d97447a
Logan Liffick <ltliffick@gmail.com>
2019-06-23 23:38:54 -0400

    updated files

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 // js //
 
 console.log('Made with 🔥 by 🤖s');
+
+let canvas;
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  noStroke();
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 100, 100);
+  fill(255, 108, 137);
+  
+if (mouseIsPressed) {
+  clear();
+}
+  
+}
