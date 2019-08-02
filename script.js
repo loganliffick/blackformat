19565331d5ab1dc8d1364382f4c8f46f2d01ea6b
Brandon Chang <brandonchang@asana.com>
2019-08-02 12:13:17 -0700

    Refactors drawing circles to drawing lines

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // js //
 
+// import {resizeCanvas} from "p5/global";
+
 console.log('🔥 Designed and developed by Logan Liffick 🔥');
 
 // p5 Animation //
@@ -8,47 +10,58 @@ let canvas;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-};
+}
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
-  noStroke();
-};
+  mousePos = createVector(mouseX, mouseY);
+  lastMousePos = createVector(mouseX, mouseY);
+  strokeWeight(100);
+}
 
 let x;
 let y;
 let col;
+let mousePos;
+let lastMousePos;
 
 function draw() {
-  
+
   col = random(0, 255);
-  
-  x = lerp(col, mouseX, .5);
-  y = lerp(col, mouseY, .5);
-  
-  color1 = map(mouseX, 0, windowWidth, 0, 255);
-  color2 = map(mouseY, 0, windowHeight, 0, 255);
-  color3 = 180;
-  
-  ellipse(mouseX, mouseY, 150, 150);
-  fill(color1, color2, color3);
-  
+
+  // Get new mouse position
+  mousePos.set(mouseX, mouseY);
+
+  // Set the colors for our new line
+  stroke(
+    map(mousePos.x, 0, windowWidth, 0, 255),
+    map(mousePos.y, 0, windowHeight, 0, 255),
+    180
+  );
+
+  if (lastMousePos.x !== 0 && lastMousePos.y !== 0) {
+    line(mousePos.x, mousePos.y, lastMousePos.x, lastMousePos.y);
+  }
+
   if (mouseIsPressed) {
     clear();
-  };
-};
+  }
+
+  // Keep track of the last position of the mouse
+  lastMousePos.set(mousePos)
+}
 
 // Waypoints //
 
 let waypoint1 = new Waypoint({
   element: document.getElementById('sec2'),
-  handler: function(direction) {
-    if (direction == 'down') {
+  handler: function (direction) {
+    if (direction === 'down') {
       document.getElementById('switch1').classList.remove('active');
       document.getElementById('switch2').classList.add('active');
-    } else if (direction == 'up') {
+    } else if (direction === 'up') {
       document.getElementById('switch1').classList.add('active');
       document.getElementById('switch2').classList.remove('active');
     }
@@ -58,11 +71,11 @@ let waypoint1 = new Waypoint({
 
 let waypoint2 = new Waypoint({
   element: document.getElementById('sec3'),
-  handler: function(direction) {
-    if (direction == 'down') {
+  handler: function (direction) {
+    if (direction === 'down') {
       document.getElementById('switch2').classList.remove('active');
       document.getElementById('switch3').classList.add('active');
-    } else if (direction == 'up') {
+    } else if (direction === 'up') {
       document.getElementById('switch2').classList.add('active');
       document.getElementById('switch3').classList.remove('active');
     }
@@ -72,11 +85,11 @@ let waypoint2 = new Waypoint({
 
 let waypoint3 = new Waypoint({
   element: document.getElementById('sec4'),
-  handler: function(direction) {
-    if (direction == 'down') {
+  handler: function (direction) {
+    if (direction === 'down') {
       document.getElementById('switch3').classList.remove('active');
       document.getElementById('switch4').classList.add('active');
-    } else if (direction == 'up') {
+    } else if (direction === 'up') {
       document.getElementById('switch3').classList.add('active');
       document.getElementById('switch4').classList.remove('active');
     }
